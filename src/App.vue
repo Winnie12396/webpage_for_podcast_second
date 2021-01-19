ad3a1ffd36bdcc2364238be00777f3eabcd27752
Winnie12396 <71814998+Winnie12396@users.noreply.github.com>
2021-01-19 23:38:01 +0800

    with toolbars and some cards

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app>
+    <div class="bar">
+      <v-app-bar
+        app
+        color="white"
+        flat
+      >
+        <v-toolbar
+          flat
+        >
+
+          <v-toolbar-title><h4>天空裡的魚 Fish in the Sky</h4></v-toolbar-title>
+
+          <v-spacer></v-spacer>
+          <v-toolbar-items>
+            <v-btn text>主頁</v-btn>
+            <v-btn text>關於</v-btn>
+            <v-btn text>所有單集</v-btn>
+            <v-btn text>文章</v-btn>
+          </v-toolbar-items>
+        </v-toolbar>
+      </v-app-bar>
+
+    </div>
+
+    <v-content>
+      <Intro/>
+      <Latest/>
+    
+    </v-content>
+    
+    
+
+    
+  </v-app>
+</template>
+
+<script>
+
+import Intro from './components/Intro.vue'
+import Latest from './components/Latest.vue'
+export default {
+  name: 'App',
+
+  components: {
+    Intro,
+    Latest
+  },
+
+  data: () => ({
+  
+  }),
+};
+</script>
+
+<style>
+
+</style>
+
---
+++ src/components/Latest.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container>
+    <div class="flex-no-wrap justify-space-between">
+
+      
+      <v-card
+        class="mx-auto"
+        max-width="336"
+      >
+        <v-img
+          class="align-end"
+          :aspect-ratio="1/1"
+          src="@/assets/logo.png"
+        >
+        </v-img>
+        <v-card-title>天空裡的魚</v-card-title>
+        <v-card-subtitle>播客</v-card-subtitle>
+
+        <v-card-text>
+          Phasellus magna. Quisque rutrum. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Aliquam lobortis. Quisque libero metus, condimentum nec, tempor a, commodo mollis, magna.
+
+          In turpis. In dui magna, posuere eget, vestibulum et, tempor auctor, justo. In turpis. Pellentesque dapibus hendrerit tortor. Ut varius tincidunt libero.
+        </v-card-text>
+      </v-card>
+      
+    </div>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Latest',
+
+    data: () => ({
+    }),
+  }
+</script>
