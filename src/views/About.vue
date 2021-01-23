1bb3272461a7a6f0be43d9583b8e61399fa9c4d3
Winnie12396 <71814998+Winnie12396@users.noreply.github.com>
2021-01-23 22:44:13 +0800

    Add some About and cover pics

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,21 @@
 <template>
   <v-content class="about">
-    <h1>This is an about page</h1>
+    <v-layout row wrap justify-center pt-12>
+      <v-flex xs12 md8>
+        <body-1>
+          <p>天空裡的魚，這個節目的起源，是一個19歲大學女生，在一次期末作業之中突發奇想，也許可以來試試看做podcast這件事。</p>
+          <p>這個作業是生物藝術概論課的期末作業，要做形式不拘的藝術普及，介紹生物藝術的作品；我的成品就是第一集的內容。</p>
+          <p>主軸是跟吃有關的科學和其他事情，配上其他心血來潮的支線劇情~</p>
+          <p>希望一個月一到兩集，寒暑假會多一點</p>
+          <p>期待與你相見！</p>
+          <p>目前在<a href="https://open.firstory.me/user/fishinthesky">Firstory</a>、<a href="https://podcast.kkbox.com/channel/P_Rw-Q1b7808oGoPbC">KKbox</a>、<a href="https://open.spotify.com/show/4cMfhxhkGo6a0xaYfakCeG">Spotify</a>、Apple Podcasts跟PocketCasts皆可收聽喔</p>
+          <p>有任何想聊的可以私訊 IG @fishinthesky1212 或寄信給我：fishinthesky1212@gmail.com</p>
+        </body-1>
+      
+      </v-flex>
+    </v-layout>
+
+
   </v-content>
 </template>
 
@@ -17,4 +32,8 @@ export default {
   }),
 };
 </script>
+<style>
+
+</style>
+
 
